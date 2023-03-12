04a6f3d8304b55cfc335ad7a0a6c6080aae31ff7
Aubrey Lamb <aubreyrlamb@gmail.com>
2023-03-11 17:04:42 -0700

    added correct starter code

--- script.js
+++ script.js
@@ -1,45 +1,35 @@
-// Assignment code here
-// I need variables here
-// var alphabetNumericCharacters = ('abcdefghijklmnopqrstuvwxyz0123456789');
-// var passwordCharacters = ("~!@#$%^&*()_+");
-// can I use math random instead of making a variable for numbers?
+var generateBtn = document.querySelector("#generate");
 const lowercase = "abcdefghijklmnopqrstuvwxyz";
 const uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 const numbers = "0123456789"; 
 const symbols = "!@#$%^&*_-+=";
 
-var options = [lowercase, uppercase, numbers, symbols];
-var index = Math.floor(Math.random() * options.length);
-var generatePassword = document.querySelector("#Password")
-var writePassword = function() { 
 
-var userChoice = window.prompt("How many characters?")
-if (userChoice > 1) {
-window.prompt(generatePassword + index.length);
 
-  } }
+function generatePassword () {
+  var length = parseInt(prompt("What is the lenght of your password"))
+  if(isNaN(length)) {
+    alert("Must choose a number");
+    return null;
+  }
 
-// Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
-// var generatePassword = document.querySelector("#generate");
-// document.getElementById("generate-password").addEventListener("click", e => {
-//   e.preventDefault();
 
+}
+
+function getRandom (x) {
+  var randomizer = Math.floor(Math.random() * x.length);
+  var randEl = x[randomizer]
+  return randEl;
+}
 
 // Write password to the #password input
-// var writePassword = function() {
-  
- 
-//   passwordText.value = password;
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
 
-// }
+  passwordText.value = password;
 
-// I need a button, I also need to create prompts and responses that will give me an output.
+}
 
-// function generatePassword() {
-//   var prompt = ("How many characters?")
-//   console.log(response) + (password.length) ; (writePassword)
-  
-// }
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
